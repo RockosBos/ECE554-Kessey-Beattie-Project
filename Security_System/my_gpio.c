b7e92c3fca832006f84c0103f1da4302b3617050
RockosBos <32206608+RockosBos@users.noreply.github.com>
2024-03-24 14:04:54 -0400

    Init state

--- Security_System/my_gpio.c
+++ Security_System/my_gpio.c
@@ -6,11 +6,11 @@ void IntPortFHandler(void)
     uint32_t status = GPIOIntStatus(GPIO_PORTF_BASE, true);
 
     if (status & SW1_INT_PIN) {
-        GPIOPinWrite(GPIO_PORTF_BASE, LED_RED_PIN, LED_RED_PIN);
+        GPIOPinWrite(GPIO_PORTF_BASE, LED_BLUE_PIN, LED_BLUE_PIN);
         UARTprintf("***SW1 was pressed***\n");
     }
     if (status & SW2_INT_PIN) {
-        GPIOPinWrite(GPIO_PORTF_BASE, LED_RED_PIN, 0);
+        GPIOPinWrite(GPIO_PORTF_BASE, LED_BLUE_PIN, 0);
         UARTprintf("***SW2 was pressed***\n");
     }
 
